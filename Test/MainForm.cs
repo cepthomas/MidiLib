860cffa48ebaf9f4520e164cef11fd329eef8764
cepthomas <cepthomas@gmail.com>
2025-05-26 16:18:04 -0500

    WIP.

--- Test/MainForm.cs
+++ Test/MainForm.cs
@@ -133,16 +133,16 @@ namespace Ephemera.MidiLib.Test
             if(ok)
             {
                 // Style file, full info:
-                OpenFile(@"C:\Dev\repos\Misc\TestAudioFiles\_LoveSong.S474.sty");
+                OpenFile(@"C:\Dev\Misc\TestAudioFiles\_LoveSong.S474.sty");
 
                 // Plain midi, full song:
-                //OpenFile(@"C:\Dev\repos\Misc\TestAudioFiles\WICKGAME.MID");
+                //OpenFile(@"C:\Dev\Misc\TestAudioFiles\WICKGAME.MID");
 
                 // Plain midi, one instrument, no patch:
-                //OpenFile(@"C:\Dev\repos\Misc\TestAudioFiles\_bass_ch2.mid");
+                //OpenFile(@"C:\Dev\Misc\TestAudioFiles\_bass_ch2.mid");
 
                 // Plain midi, one instrument, ??? patch:
-                //OpenFile(@"C:\Dev\repos\Misc\TestAudioFiles\_drums_ch1.mid");
+                //OpenFile(@"C:\Dev\Misc\TestAudioFiles\_drums_ch1.mid");
             }
             base.OnLoad(e);
         }
